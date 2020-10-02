623686b54c4756b3bf71246fac275f944c55a194
chloelangston <chloealangston@gmail.com>
2020-10-02 15:51:42 +0000

    styling changes

--- sketch.js
+++ sketch.js
@@ -1,15 +1,7 @@
 
-// Check if user is at the top of the page
-function isAtTop() {
-    if(window.scrollY==0){
-        document.getElementById("graphic-1").classList.remove("img-fade");
-    } else {
-        checkElementsInView();
-    }
-}
-
 // Scrolling functionality
 function elementInViewport(el) {
+    // console.log(document.scrollingElement.scrollTop)
     var top = el.offsetTop;
     var left = el.offsetLeft;
     var width = el.offsetWidth;
@@ -30,13 +22,14 @@ function elementInViewport(el) {
 }
 
 function checkElementsInView() {
+    var directions = document.getElementById("directions");
     var element1 = document.getElementById("text-1");
     var element2 = document.getElementById("text-2");
     var element3 = document.getElementById("text-3");
     var element4 = document.getElementById("text-4");
     var elementEnd = document.getElementById("text-end");
 
-    if (elementInViewport(element1)) {
+    if (elementInViewport(directions) || elementInViewport(element1)) {
         document.getElementById("graphic-1").classList.remove("img-fade");
         document.getElementById("graphic-2").classList.add("img-fade");
     }
@@ -69,12 +62,12 @@ function checkElementsInView() {
         document.getElementById("graphic-4").classList.add("img-fade");
     }
 
-    // else {
-    //     document.getElementById("graphic-1").classList.add("img-fade");
-    //     document.getElementById("graphic-2").classList.add("img-fade");
-    //     document.getElementById("graphic-3").classList.add("img-fade");
-    //     document.getElementById("graphic-4").classList.add("img-fade");
-    // }
+    else {
+        document.getElementById("graphic-1").classList.add("img-fade");
+        document.getElementById("graphic-2").classList.add("img-fade");
+        document.getElementById("graphic-3").classList.add("img-fade");
+        document.getElementById("graphic-4").classList.add("img-fade");
+    }
 }
 
 
@@ -115,7 +108,7 @@ var myFunction = function(elem) {
     	elem.parentElement.previousElementSibling.classList.add("hide");
     }
 
-    nextDecisionElem.scrollIntoView({behavior: "smooth", block: "start", inline: "nearest"});
+    nextDecisionElem.scrollIntoView({behavior: "smooth", block: "center", inline: "nearest"});
 };
 
 for (var i = 0; i < processElements.length; i++) {
@@ -131,5 +124,5 @@ function start() {
 	document.getElementById("start").classList.add("fade");
 	document.getElementById("start").classList.add("no-hover");
 
-    document.getElementById("start").nextElementSibling.scrollIntoView({behavior: "smooth", block: "start", inline: "nearest"});
+    document.getElementById("start").nextElementSibling.scrollIntoView({behavior: "smooth", block: "center", inline: "nearest"});
 }
